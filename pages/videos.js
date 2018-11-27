2a7496f6b1499e3811ce15b7172374b97729098a
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-27 22:28:12 +0700

    no message

---
+++ pages/videos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from '../components/card'
+
+export default class Videos extends React.Component {
+    render() {
+        return (<div>
+            <Card></Card>
+        </div>)
+    }
+}
