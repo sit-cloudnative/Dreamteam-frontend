e9bd66ef1de05f69588c1817ddb8f689146bcf88
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-27 22:09:17 +0700

    [ADD] page of video

---
+++ pages/video.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Video from '../components/video'
+
+export default class Test extends React.Component {
+    render() {
+        return (<div>
+            <Video></Video>
+        </div>)
+    }
+}
