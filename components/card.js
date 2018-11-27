40a3714eb36bf93146ba37b75e70b122130ed8fd
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-27 21:02:50 +0700

    [ADD] Card

--- components/card.js
+++ components/card.js
@@ -1,17 +1,76 @@
-// import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-// class Card extends React.Component{
-//     render() {
-//         return(<div>
-//             <h1>Dreateam new Front end!</h1>
-            
-//         </div>)
-//     }
-// }
-// export default Card
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
 
-const Card = (props) => (
-    <div>Card</div>
-)
 
-export default Card
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              T
+            </Avatar>
+          }
+
+          title="Olarn"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image="https://image.ibb.co/dG2XCA/boy.png"
+          title="Paella dish"
+        />
+        <CardContent>
+
+        </CardContent>
+
+
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
