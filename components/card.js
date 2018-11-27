6a8dec58a023679618440fa63f7c9207ce17d9a2
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-27 17:13:15 +0700

    [ADD] Card

---
+++ components/card.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// class Card extends React.Component{
+//     render() {
+//         return(<div>
+//             <h1>Dreateam new Front end!</h1>
+            
+//         </div>)
+//     }
+// }
+// export default Card
+
+const Card = (props) => (
+    <div>Card</div>
+)
+
+export default Card
