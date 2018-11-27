30f92fcda1c5183966db4fde1e768c863f86e5b1
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-27 23:44:04 +0700

    [ADD] Login page and component v1

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Login from '../../components/login'
+import AppBar from '../../components/appbar'
+import SearchBar from 'material-ui-search-bar'
 
-export default class extends React.Component{
+
+export default class index extends React.Component {
     render() {
-        return(<div>
-            <h1>Login</h1>
+        return (<div>
+            <AppBar></AppBar>
             <Login></Login>
+
+            <SearchBar
+                onChange={() => console.log('onChange')}
+                onRequestSearch={() => console.log('onRequestSearch')}
+                style={{
+                    margin: '0 auto',
+                    maxWidth: 800
+                }}
+            />
         </div>)
     }
 }
