8363f2d426078396815c16db7556d0c6fdc2a54e
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-27 20:21:25 +0700

    no message

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from '../components/card'
+
+export default class Test extends React.Component {
+    render() {
+        return (<div>
+            <Card></Card>
+        </div>)
+    }
+}
