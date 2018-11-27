9114188df594a5ce4c03f7b65211ab4611cc668c
divinize <patric_emmel@outlook.com>
2018-11-27 16:53:58 +0700

    [ADD]index page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Index extends React.Component{
+    render() {
+        return(<div>
+            <h1>Dreateam new Front end!</h1>
+        </div>)
+    }
+}
