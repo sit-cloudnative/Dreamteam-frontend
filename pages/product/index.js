b512ca3a11f4e3befa30faa355774c765c9a5f20
divinize <patric_emmel@outlook.com>
2018-11-28 17:59:41 +0700

    [UPDATE]change front-end framework to reactstrap

---
+++ pages/product/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Index extends React.Component{
+    render() {
+        return(<div>
+            <h1>This is the productPage!</h1>
+        </div>)
+    }
+}
