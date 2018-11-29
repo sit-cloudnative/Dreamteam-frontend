2e8880929618ec420abafb7bba6096f20d0e0531
divinize <patric_emmel@outlook.com>
2018-11-29 15:17:14 +0700

    [ADD]fetch profile from userSErvice

--- components/profilecard.js
+++ components/profilecard.js
@@ -1,12 +1,9 @@
 import { Form, FormGroup, Label, Input } from 'reactstrap';
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import {Card,CardBody,Button} from 'reactstrap';
 import { Row, Col } from 'reactstrap';
 
 
-export default (props) => (
+export default ({profile}) => (
     <Card>
         {/* <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" /> */}
         <CardBody>
@@ -15,23 +12,23 @@ export default (props) => (
             <Form>
                 <FormGroup>
                     <Label for="firstname">Firstname</Label>
-                    <Input type="text" name="firstname" value="Tanapat" disabled />
+                    <Input type="text" name="firstname" value={profile.firstname} readOnly />
                 </FormGroup>
                 <FormGroup>
                     <Label for="lastname">Lastname</Label>
-                    <Input type="text" name="lastname" id="lastname" value="Choochot" disabled />
+                    <Input type="text" name="lastname" id="lastname" value={profile.lastname} readOnly />
                 </FormGroup>
                 <FormGroup>
                     <Label for="Year">Year</Label>
-                    <Input type="number" name="year" id="Year" value="3" disabled />
+                    <Input type="number" name="year" id="Year" value={profile.year} readOnly />
                 </FormGroup>
                 <FormGroup>
                     <Label for="Department">Department</Label>
-                    <Input type="text" name="department" id="department" value="IT" disabled />
+                    <Input type="text" name="department" id="department" value={profile.department} readOnly />
                 </FormGroup>
                 <FormGroup>
                     <Label for="Faculty">Faculty</Label>
-                    <Input type="text" name="faculty" id="faculty" value="SIT" disabled />
+                    <Input type="text" name="faculty" id="faculty" value={profile.faculty} readOnly />
                 </FormGroup>
                 <Row>
                     <Col style={{ textAlign: 'center' }}>
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,22 +1,56 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap';
-
+import {userService} from '../../util/axios'
 import Profileinfo from '../../components/profileinfo'
 import ProfileCard from '../../components/profilecard'
 import Template from '../../layout/template'
 export default class Index extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            profile: {
+                studentId: '',
+                firstname: '',
+                lastname: '',
+                favoriteSubject: [
+                    {
+                        subjectId: 0,
+                        subjectName: ''
+                    }
+                ]
+            },
+            isLoading:true
+        }
+        this.axios = {}
+    }
+
+    async componentDidMount() {
+        let studentId  = localStorage.getItem('profileId')
+        let jwtToken  = localStorage.getItem('token')
+        this.axios = userService(jwtToken)
+        let { data } = await this.axios.get(`/user/${studentId}`)
+        this.setState({
+            profile: data,
+            isLoading:false
+        })
+        console.log(this.state.profile)
+    }
+
     render() {
         return (
             <Template>
                 <div className='container'>
-                    <Row style={{ paddingTop: '20px' }}>
-                        <Col xs="4" style={{ textAlign: 'center' }}>
-                            <Profileinfo />
-                        </Col>
-                        <Col xs='7'>
-                            <ProfileCard/> 
-                        </Col>
-                    </Row>
+                    {this.state.isLoading ? 'loading...' :
+                        <Row style={{ paddingTop: '20px' }}>
+                            <Col xs="4" style={{ textAlign: 'center' }}>
+                                <Profileinfo profile={this.state.profile}/>
+                            </Col>
+                            <Col xs='7'>
+                                <ProfileCard profile={this.state.profile}/> 
+                            </Col>
+                        </Row>
+                    }
                 </div>
             </Template >)
     }
