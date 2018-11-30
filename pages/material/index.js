ef9a519da9c685822cbe5739ba07f387ca4c6d02
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-30 23:43:14 +0700

    [UPDATE] material / now can back to video

--- pages/material/index.js
+++ pages/material/index.js
@@ -3,26 +3,62 @@ import AdminTable from '../../components/admintable'
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import Template from '../../layout/template'
 import UploadModal from '../../components/uploadmodal'
+import Router, { withRouter } from 'next/router'
+import { subjectService } from '../../util/axios'
 
+class Index extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            courseName: ''
+        }
+        this.axios = {}
 
-export default class Index extends React.Component{
+        this.onBackToSubject = this.onBackToSubject.bind(this)
+    }
+    async componentDidMount() {
+        const { router } = this.props
+        const courseName = router.query.subject_code
+        await this.setState({
+            courseName
+        })
+        console.log(this.state.courseName)
+    }
+    async onBackToSubject() {
+        const token = ''
+        this.axios = subjectService(token)
+        const { data } = await this.axios.get('/subjects',{
+            params: {
+                keyword: this.state.courseName
+            }
+        })
+        const subject = data[0]
+        console.log(subject)
+        Router.push({
+            pathname: '/videos',
+            query: {
+                subject_id: subject.subjectId
+            }
+        })
+    }
     render() {
         return(
         <Template>
             <Breadcrumb>
                 <BreadcrumbItem><a href="#">B.SC.IT</a></BreadcrumbItem>
-                <BreadcrumbItem active><a href="">INT102</a></BreadcrumbItem>
+                <BreadcrumbItem active><a onClick={this.onBackToSubject} style={{cursor: 'pointer'}}>{this.state.courseName}</a></BreadcrumbItem>
                 <BreadcrumbItem>Material</BreadcrumbItem>
             </Breadcrumb>
             <h2>Material</h2>
             <div className='row' style={{paddingBottom:'22px'}}>
                 <div className='col-2 offset-10'>
-                    <UploadModal />
+                    <UploadModal courseName={this.state.courseName}/>
                 </div>
             </div>
             <div className="container">
-                <AdminTable />
+                <AdminTable courseName={this.state.courseName}/>
             </div>
         </Template>)
     }
 }
+export default withRouter(Index)
