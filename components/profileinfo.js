3ff5cfdf41a2bb5c9aa1212d1d27fc0a862e544e
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-30 16:02:10 +0700

    [UPDATE] change to Default user icon

--- components/profileinfo.js
+++ components/profileinfo.js
@@ -1,6 +1,6 @@
 export default ({profile}) => (
     <div>
-        <img style={{ borderRadius: '50%' }} src="../../static/images/avatar/user1.jpg" width="180" height="171" />
+        <img style={{ borderRadius: '50%' }} src="../../static/images/logo/user-1.png" width="180" height="171" />
         <h3>{profile.firstname}  {profile.lastname}</h3>
         <h6>@{profile.username}</h6>
         <hr />
