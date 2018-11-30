45c64730055007bb608a1a813766db69406f8563
PATRIC EMMEL <patric.emmel@mail.kmutt.ac.th>
2018-11-30 19:46:08 +0700

    [ADD] react loader generic

---
+++ components/materialsCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table from './table'
+import {Card} from 'reactstrap'
+export default () => (
+    <div>
+        <Card style={{
+            width:'1200px',
+            textAlign:'center'
+        }}>
+            <Table/>
+        </Card>
+    </div>
+)
