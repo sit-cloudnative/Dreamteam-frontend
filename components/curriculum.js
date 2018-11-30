8d1d3afc7a8970fc04095301a6ec8f1a3bf796a8
Pattharathon srithundorn <pattharathon.srithundorn@mail.kmutt.ac.th>
2018-11-30 23:24:19 +0700

    [UPDATE] text overflow and button size

--- components/curriculum.js
+++ components/curriculum.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import {subjectService} from '../util/axios'
+import { subjectService } from '../util/axios'
 import { Container } from 'reactstrap';
 import Router from 'next/router'
 import Spiner from '../components/loadingcomponent'
@@ -12,8 +12,8 @@ export default class Curriculum extends React.Component {
             curriculum: [],
             subjectList: '',
             curriculumCode: '',
-            isLoading:true,
-            isSubjectLoading:true
+            isLoading: true,
+            isSubjectLoading: true
         }
         this.axios = {}
         this.getSubjectList = this.getSubjectList.bind(this)
@@ -26,7 +26,7 @@ export default class Curriculum extends React.Component {
         let { data } = await this.axios.get('/curriculums')
         this.setState({
             curriculum: data,
-            isLoading:false
+            isLoading: false
         })
         console.log(this.state.curriculum)
     }
@@ -46,56 +46,57 @@ export default class Curriculum extends React.Component {
     }
     render() {
         const cardStyle = {
-            height:'539px',
-            overflowY:'scroll',
-            overflowX:'hidden'
+            width: '613px',
+            height: '539px',
+            overflowY: 'scroll',
+            overflowX: 'hidden'
         };
         const cardtitleStyle = {
             fontSize: '33px',
             textAlign: 'center'
         };
-        const message ={
-            width:'100%',
-            height:'100%',
+        const message = {
+            width: '100%',
+            height: '100%',
             minHeight: '400px',
-            display:'flex',
-            display:'-webkit-flex',
-            flexWrap:'wrap',
-            flexDirection:'row',
-            justifyContent:'center',
-            alignItems:'center',
-            alignContent:'center',
-            color:'#d9d9d9',
-            fontSize:'30px'
+            display: 'flex',
+            display: '-webkit-flex',
+            flexWrap: 'wrap',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
+            color: '#d9d9d9',
+            fontSize: '30px'
         }
 
         return (
-            <Container style={{marginTop:'11px'}} fluid>
-                <Row>   
-                    <Col sm="6">
+            <Container style={{ marginTop: '11px' }} fluid>
+                <Row>
+                    <Col sm="1"></Col>
+                    <Col sm="5">
                         <Card style={cardStyle} body>
                             <CardTitle style={cardtitleStyle}><i className="fas fa-graduation-cap"></i> Curriculum</CardTitle>
                             {
-                            (this.state.isLoading)? <Spiner /> :this.state.curriculum.map( (c, key) => (
-                                <Button className="btn" onClick={() => {this.getSubjectList(c.curriculumId)}} key={key} style={{marginTop:'11px',textAlign:'left' ,backgroundColor:'#0091ac' ,width:'555px'}}> <i className="fas fa-graduation-cap fa-2x"></i>{c.curriculumName}</Button>
-                                )) 
+                                (this.state.isLoading) ? <Spiner /> : this.state.curriculum.map((c, key) => (
+                                    <Button className="btn " onClick={() => { this.getSubjectList(c.curriculumId) }} key={key} style={{ paddingBottom: '35px', marginTop: '11px', textAlign: 'left', backgroundColor: '#0091ac', width: '555px', textOverflow: 'ellipsis', overflow: 'hidden' }}> <i className="fas fa-graduation-cap fa-2x"></i>{c.curriculumName}</Button>
+                                ))
                             }
-                            
                         </Card>
                     </Col>
                     <Col sm="6">
-                        <Card body style={cardStyle}>
+                        <Card style={cardStyle} body>
                             <CardTitle style={cardtitleStyle}><i className="fas fa-book fa-1x"></i> Subject</CardTitle>
                             {this.state.subjectList == '' ? (<div style={message}>
                                 <i style={{
-                                    marginRight:'15px'
+                                    marginRight: '15px'
                                 }} className="fas fa-arrow-left"></i>Please select a curriculum
-                            </div>):(
-                                this.state.subjectList.map( (s,key) => (
-                                    <Button className="btn" onClick={()=> {this.redirectToVideoListPage(s.subjectId)}} key={key} style={{marginTop:'11px',textAlign:'left' ,backgroundColor:'#0091ac',width:'555px' }}> <i className="fas fa-graduation-cap fa-2x"></i>{s.subjectName}</Button>
-                                ))
-                            )}
-                            
+                            </div>) : (
+                                    this.state.subjectList.map((s, key) => (
+                                        <Button className="btn" onClick={()=> {this.redirectToVideoListPage(s.subjectId)}} key={key} style={{paddingBottom:'35px', marginTop:'11px',textAlign:'left' ,backgroundColor:'#0091ac',width:'555px'}}> <i className="fas fa-graduation-cap fa-2x"></i>{s.subjectName}</Button>
+                                    ))
+                                )}
+
                         </Card>
                     </Col>
                 </Row>
--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
             font: 11px menlo;
           }
           .navbar {
+            color: #F7F9FB;
             background-color: #31708E;
             webkit-box-shadow: 0px 2px 11px -3px rgba(0,0,0,0.75);
             moz-box-shadow: 0px 2px 11px -3px rgba(0,0,0,0.75);
@@ -79,6 +80,14 @@ export default class MyDocument extends Document {
           webkit-box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
           moz-box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
           box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
+        }
+        .btn-secondary :hover {
+          webkit-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
+          moz-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
+          box-shadow: 3px 4px 7px 5px rgba(0,0,0,0.1);
+          -ms-transform: scale(1); 
+          -webkit-transform: scale(1);
+          transform: scale(1); 
         }
         `}
         </style>
