6149eae3672e4f32dbe538da1024cf044580d88d
divinize <patric.emmel@outlook.com>
2018-11-30 22:01:21 +0700

    [ADD] add video loading

--- components/cardlist.js
+++ components/cardlist.js
@@ -6,30 +6,16 @@ import {videoService} from '../util/axios'
 export default class Cardlist extends React.Component {
     constructor(props){
         super(props)
-        this.state = {
-            targetSubject:'',
-            videoList: [],
-            subjectName: '',
-        }
         this.axios = {}
     }
     
-    async componentDidMount () {
-        let subjectId = this.props.subjectId
-        let token = localStorage.getItem('token')
-        this.axios  = videoService(token)
-        let {data} = await this.axios.get(`/subject/${subjectId}/videos`)
-        this.setState({videoList:data})
-        let subjectTitle = data[0].videoName
-        this.props.changeVideoTitle(subjectTitle)
-        console.log()
-    }
+
     render() {
         return (
     <div>
         <Container>
             <Row>
-                {this.state.videoList.map((video,key) =>(
+                {this.props.videoList.map((video,key) =>(
                 <Col xs='3' key={key}>
                     <Card video={video}/>
                 </Col>
