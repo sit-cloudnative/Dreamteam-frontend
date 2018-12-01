7750b2250e1b8ef4699737e9b8ce2235408146a9
divinize <patric.emmel@outlook.com>
2018-12-01 12:34:20 +0700

    [ADD] deploy script

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        '/login': { page: '/login' },
+        '/user': { page: '/user' },
+        '/subject': { page: '/subject' },
+        '/videos': { page: '/videos' },
+        '/video': { page: '/video' },
+        '/material': { page: '/material' },
+        '/oops': { page: '/oops' },
+      }
+    }
+  }
