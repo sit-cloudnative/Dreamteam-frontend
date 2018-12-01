2ad48b671dccca417f0442c7d41557ed71b67a8a
divinize <patric.emmel@outlook.com>
2018-12-01 08:54:12 +0700

    [ADD]redirect login route

--- layout/template.js
+++ layout/template.js
@@ -1,13 +1,33 @@
 import Meta from './meta'
 import Footer from './footer'
 import Navbar from '../components/navbar'
-export default ({ children }) => (
-  <div>
-    <Meta />
-    <Navbar/>
-    <div style={{padding:'8px',marginTop:'55px',backgroundColor:'#f7f7f7'}}>
-      { children }
+import React from 'react'
+import Router from 'next/router'
+export default class Template extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount(){
+    let token = localStorage.getItem('token')
+    console.log('token')
+    console.log(token)
+    if(token == null ){
+      Router.push('/login')
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Meta />
+        <Navbar/>
+        <div style={{padding:'8px',marginTop:'55px',backgroundColor:'#f7f7f7'}}>
+          { this.props.children }
+        </div>
+        <Footer />
       </div>
-    <Footer />
-  </div>
-)
+    )
+  }
+}
