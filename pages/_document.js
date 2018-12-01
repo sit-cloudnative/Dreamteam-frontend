369a68bec5070d9f1d7e15b8616c5ec4798c4b6d
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-12-01 13:35:26 +0700

    [DELETE] .btn-secondary :hover in _document

--- pages/_document.js
+++ pages/_document.js
@@ -66,14 +66,6 @@ export default class MyDocument extends Document {
           -webkit-transition: width 0.4s ease-in-out;
           transition: width 0.4s ease-in-out;
           }
-          .btn-secondary :hover {
-            webkit-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
-            moz-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
-            box-shadow: 3px 4px 7px 5px rgba(0,0,0,0.1);
-            -ms-transform: scale(1); 
-            -webkit-transform: scale(1);
-            transform: scale(1); 
-          }
         button.backToHome {
           background-color: #31708E;
         }
@@ -85,14 +77,6 @@ export default class MyDocument extends Document {
           webkit-box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
           moz-box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
           box-shadow: 2px 3px 5px 0px rgba(0,0,0,0.1);
-        }
-        .btn-secondary :hover {
-          webkit-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
-          moz-box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.1);
-          box-shadow: 3px 4px 7px 5px rgba(0,0,0,0.1);
-          -ms-transform: scale(1); 
-          -webkit-transform: scale(1);
-          transform: scale(1); 
         }
         .footer {
           padding-top: 5px;
